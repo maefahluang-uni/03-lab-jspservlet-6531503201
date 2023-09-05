d5e1af8dd1006f438e19c3a60158441a4624470d
jemiezler <142890376+jemiezler@users.noreply.github.com>
2023-09-05 12:51:15 +0700

    Finally!!!!!!!!!!!!!

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -8,21 +8,48 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-//TODO: add webservlet to "/calbmi"
+@WebServlet("/calbmi") //TODO: add webservlet to "/calbmi"
 public class BMICalculatorServlet extends HttpServlet{
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //TODO: get parameter from request: "weight" and "height"
         
+        //TODO: get parameter from request: "weight" and "height"
+        double weight = Double.parseDouble(request.getParameter("weight"));
+        double height = Double.parseDouble(request.getParameter("height"));
+
         //TODO: calculate bmi
+        double bmi = calculateBMI(weight, height);
 
         //TODO: determine the built from BMI
+        String build = determineBuild(bmi);
       
         //TODO: add bmi and built to the request's attribute
+        request.setAttribute("bmi", bmi);
+        request.setAttribute("build", build);
 
         //TODO: forward to jsp
-           
+        request.getRequestDispatcher("/bmi_result.jsp").forward(request, response);
     }
-    
+
+    //Calculate BMI
+    private double calculateBMI(double weight, double height) {
+        // BMI Formula: BMI = weight (kg) / (height (m) * height (m))
+        return weight / (height * height);
+    }
+
+    // Determine the build from BMI
+    private String determineBuild(double bmi) {
+        if (bmi < 18.5) {
+            return "Underweight";
+        } else if (bmi >= 18.5 && bmi < 25) {
+            return "Normal";
+        } else if (bmi >= 25 && bmi < 30) {
+            return "Overweight";
+        } else if (bmi >= 30 && bmi < 35) {
+            return "Obese";
+        } else {
+            return "Extreamly Obese";
+        }
+    }
 }
--- src/test/java/th/mfu/TestBMICalculatorIT.java
+++ src/test/java/th/mfu/TestBMICalculatorIT.java
@@ -64,7 +64,25 @@ public class TestBMICalculatorIT {
             String jsonResponse = response.readEntity(String.class);
             assertThat(jsonResponse, CoreMatchers.containsString("Result is 22"));
 
-            assertThat(jsonResponse, CoreMatchers.containsString("normal"));
+            assertThat(jsonResponse, CoreMatchers.containsString("Normal"));
+            _logger.info("IT1 test passed");
+        }
+    }
+
+    @Test
+    public void testCaclulate3() {
+
+        // Make a HTTP GET request to retrieve the last created Parolee.
+        try (Response response = client.target(WEB_URI+"?weight=75&height=1.75").request().get()) {
+
+            // Check that the HTTP response code is 200 OK.
+            int responseCode = response.getStatus();
+            assertEquals(200, responseCode);
+
+            String jsonResponse = response.readEntity(String.class);
+            assertThat(jsonResponse, CoreMatchers.containsString("Result is 24"));
+
+            assertThat(jsonResponse, CoreMatchers.containsString("Normal"));
             _logger.info("IT1 test passed");
         }
     }
